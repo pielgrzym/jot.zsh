0f43e9eed7f4073bf49eafb6019c5a229178138b
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2013-01-01 12:17:41 +0100

    Automatically append zsh to jots' names

--- jot.zsh
+++ jot.zsh
@@ -20,12 +20,12 @@ jot_temp(){
         trap "rm -f '$TMPFILE'" 0
         echo -n "Jotfile name: "
         read name
-        if [[ -a $JOTTEMPDIR/$name ]]; then
+        if [[ -a $JOTTEMPDIR/$name.zsh ]]; then
                 echo -n "Jotfile $name already exists. Choose a new name: "
                 read name
         fi
         if [[ $name != "" ]]; then
-                jotpath=$JOTTEMPDIR/$name
+                jotpath=$JOTTEMPDIR/$name.zsh
                 mv $TMPFILE $jotpath
         else
                 tempname=$(basename "$TMPFILE")
@@ -51,14 +51,14 @@ jot(){
         trap "rm -f '$TMPFILE'" 0
         echo -n "Jotfile name: "
         read name
-        if [[ -a $JOTDIR/$name ]]; then
+        if [[ -a $JOTDIR/$name.zsh ]]; then
                 echo -n "Jotfile $name already exists. Choose a new name: "
                 read name
         fi
         if [[ $name != "" ]]; then
-                jotpath=$JOTDIR/$name
+                jotpath=$JOTDIR/$name.zsh
                 mv $TMPFILE $jotpath
-                git_add_file $JOTDIR $name
+                git_add_file $JOTDIR $name.zsh
         else
                 tempname=$(basename "$TMPFILE")
                 echo "No name given. Using $tempname"
